15955373a52b168e30456d80b2d16b3e54823a25
LuxTheCat <pertik57@gmail.com>
2018-08-11 01:16:20 +0200

    added sun and moon movement

---
+++ Assets/RotateAround.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour {
+
+    public float speed;
+
+    [SerializeField]
+    private GameObject PointObject;
+    private Vector3 RotateAroundThis;
+
+
+	void Start () {
+        RotateAroundThis = PointObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(RotateAroundThis, Vector3.right, speed * Time.deltaTime);
+        transform.LookAt(RotateAroundThis);
+	}
+}
